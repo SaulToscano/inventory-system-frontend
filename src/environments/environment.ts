b028a0cd637782420ed52bcb5f69df30445abd78
SaulToscano <saul.toscano.robles@gmail.com>
2026-09-18 14:36:19 -0700

    feat: github workflow implementation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'http://129.146.69.168:8080/api/v1',
   supabaseUrl: 'https://hrblmsycgjyzujbxwjtm.supabase.co',
   supabaseKey: 'sb_publishable_IkDMONh_dvHXs4yvOMyZYQ_xiQs57vN'
 };
